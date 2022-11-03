3654074f716100970d2084855fa99f3e54452a00
Denikaev Emil <edenikaev@mail.ru>
2022-11-03 22:12:04 +0400

    верстка, форма и мелкие исправления

--- src/app/components/office/dashboard/dashboard.component.ts
+++ src/app/components/office/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
+  analyticForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.analyticForm = new FormGroup({
+      okved: new FormControl(null, [Validators.required]),
+      tnved: new FormControl(null, [Validators.required]),
+      period: new FormControl(null, [Validators.required]),
+      region: new FormControl(null, [Validators.required]),
+    })
   }
+   submit() {
+    
+    if (this.analyticForm.invalid){
+      return
+    }
 
+    this.submitted = true
+    
+
+    // Сделано пока нет подключения к БД (POST сервер)
+    this.router.navigate(['/office', 'analytics'])
+
+  }
 }
--- src/app/components/office/office.module.ts
+++ src/app/components/office/office.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
+import { AnalyticsComponent } from "./analytics/analytics.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
 import { OfficeLayoutComponent } from "./shared/components/office-layout/office-layout.component";
@@ -11,7 +12,8 @@ import { AuthService } from "./shared/services/auth.service";
 @NgModule({
   declarations: [
     OfficeLayoutComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +25,8 @@ import { AuthService } from "./shared/services/auth.service";
         path: '', component: OfficeLayoutComponent, children: [
           {path: '', redirectTo: '/office/login', pathMatch: "full"},
           {path: 'login', component: LoginComponent},
-          {path: 'dashboard', component: DashboardComponent}
+          {path: 'dashboard', component: DashboardComponent},
+          {path: 'analytics', component: AnalyticsComponent},
         ]
       }
     ])
