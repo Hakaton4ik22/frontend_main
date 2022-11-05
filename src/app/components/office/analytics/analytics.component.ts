252a63962810823ecd47a8ecf5bf1de966a3ed1d
Denikaev Emil <edenikaev@mail.ru>
2022-11-06 00:14:52 +0400

    Добавил фильтрацию таблицы

--- src/app/components/office/analytics/analytics.component.ts
+++ src/app/components/office/analytics/analytics.component.ts
@@ -48,7 +48,10 @@ export class AnalyticsComponent implements OnInit {
     })
   }
 
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
    submit() {
     
